c5553ef700dd164da6e996ec0bf9567a817ac54f
sididiatta <juniolilwyz@gmail.com>
2022-09-27 20:35:15 +0100

    ajout de third.txt

---
+++ main.js
@@ -0,0 +1,38 @@
+const creatNewProjectCard = (title, imageUrl, text) => {
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+
+    const cardTitle = document.createElement('h3')
+    cardTitle.classList.add('card__title')
+    cardTitle.innerText = title
+    newCard.appendChild('cardTitle')
+
+    const cardImage = document.createElement('img')
+    cardImage.classList.add('card__img')
+    cardImage.src = imageUrl
+    cardImage.alt = ''
+    newCard.appendChild('cardImage')
+
+    const cardDescription = document.createElement('p')
+    cardDescription.classList.add('card__text')
+    cardDescription.innerText = text
+    newCard.appendChild('cardDescription')
+
+    return newCard
+}
+const resetForm = () => {
+    document.querySelector('#project-title').value = ''
+    document.querySelector('#project-img-link').value = ''
+    document.querySelector('#project-text').value
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    const formTitle = document.querySelector('#project-tittle').value
+    const formImageUrl = document.querySelector('#project-img-link').value
+    const formText = document.querySelector('#project-text').value
+    const newProjectCard = creatNewProjectCard(formTitle, formImageUrl, formText)
+    document.querySelector('cards').appendChild(newProjectCard)
+    resetForm()
+
+}
